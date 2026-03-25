2c6871e8063a83446d9f3c0a25906c9e2de4fac7
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2026-03-25 18:07:28 +0800

    update

--- src/PdfEditor/Program.cs
+++ src/PdfEditor/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace PdfEditor;
@@ -7,6 +8,16 @@ internal static class Program
     [STAThread]
     private static void Main()
     {
+        // 含中文等 CJK 编码的 PDF 会引用 UniGB-UTF16-H 等 CMap；资源在 itext.font_asian 中，需尽早加载该程序集
+        try
+        {
+            Assembly.Load(new AssemblyName("itext.font_asian"));
+        }
+        catch
+        {
+            // 未部署 font-asian 包时忽略；此时打开部分中文 PDF 仍可能失败
+        }
+
         ApplicationConfiguration.Initialize();
         Application.Run(new MainForm());
     }
